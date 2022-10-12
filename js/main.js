fe6b074880b6c53d28ea54b522ea800e8cef3926
Kurmankozhoev Aidarbek <aidarbek.kurmankojoev.pls@gmail.com>
2022-10-12 12:32:23 +0600

    черновик сайта автошколы

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $(".slider").slick({
+        slidesToShow: 3,
+        speed: 1000,
+        easing: "linear",
+        draggable: false,
+        waitForAnimate: false,
+    });
+});
